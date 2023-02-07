8c8872b6d3602dd40e2142ceef61fcf77f966140
darkomihic <70722923+darkomihic@users.noreply.github.com>
2023-02-07 19:19:22 +0100

    first commit
    first commit

---
+++ ooit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mihic
+ *
+ */
+module ooit {
+}
---
+++ ooit/src/ooit/nesto.java
@@ -0,0 +1,10 @@
+package ooit;
+
+public class nesto {
+	public static void main(String[] args)
+    {
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("GeeksforGeeks");
+    }
+}
