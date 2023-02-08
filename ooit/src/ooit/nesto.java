fc2187d8676a1a2dc1176df249bfbc58c704523c
darkomihic <70722923+darkomihic@users.noreply.github.com>
2023-02-08 01:00:15 +0100

    poiint
    uradio za point nesto

--- ooit/src/ooit/nesto.java
+++ ooit/src/ooit/nesto.java
@@ -1,11 +1,17 @@
 package ooit;
 
+import java.util.Scanner;
+
 public class nesto {
 	public static void main(String[] args)
     {
-        System.out.println("Welcome");
-        System.out.println("To");
-        System.out.println("GeeksforGeeks");
-        System.out.println("AJDEEEE");
+        int broj1=12;
+        int broj2=24;
+        
+        String operacija = "+";
+        
+        if(operacija=="+") {
+        	System.out.println(broj1+broj2);
+        }
     }
 }
